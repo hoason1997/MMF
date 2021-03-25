249f673c6eae5171e91ddbba5bfdc387c13953bb
hoason1997 <47147794+hoason1997@users.noreply.github.com>
2021-03-25 15:29:06 +0700

    basic finish

---
+++ MFF.WEB/Areas/SmartLab/Controllers/qtht/QTHTCauHinhChucNangController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MFF.WEB.Controllers
+{
+    [Area("SmartLab")]
+    public class QTHTCauHinhChucNangController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddForm()
+        {
+            return View();
+        }
+    }
+}
---
+++ MFF.WEB/Areas/SmartLab/Controllers/qtht/QTHTDanhMucChucNangController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MFF.WEB.Controllers
+{
+    [Area("SmartLab")]
+    public class QTHTDanhMucChucNangController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddForm()
+        {
+            return View();
+        }
+        public IActionResult AddChildForm()
+        {
+            return View();
+        }
+    }
+}
